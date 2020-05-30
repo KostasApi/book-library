72ad4ce79490c078e44ccaaad2b37c843245d0d9
KostasApi <kapidopo@hotmail.com>
2020-05-30 11:50:41 +0300

    add timestamps to book model

--- server/models/Book.model.js
+++ server/models/Book.model.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please add a title"],
-    unique: true,
-    trim: true,
-    maxlength: [50, "Title can not be more than 50 characters"],
-    minlength: [2, "Title can not be less than 2 characters"],
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Title can not be more than 50 characters"],
+      minlength: [2, "Title can not be less than 2 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description"],
+      maxlength: [500, "Description can not be more than 500 characters"],
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: [true, "Please add an author"],
+      trim: true,
+      maxlength: [50, "Author can not be more than 30 characters"],
+      minlength: [2, "Author can not be less than 2 characters"],
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Please add a description"],
-    maxlength: [500, "Description can not be more than 500 characters"],
-    trim: true,
-  },
-  author: {
-    type: String,
-    required: [true, "Please add an author"],
-    trim: true,
-    maxlength: [50, "Author can not be more than 30 characters"],
-    minlength: [2, "Author can not be less than 2 characters"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Book", BookSchema);
