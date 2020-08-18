339db56e10e4d38791d124a61a335658c4278ca3
KostasApi <kapidopo@hotmail.com>
2020-08-18 22:08:16 +0300

    move user reducer to separate file

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,68 +1,10 @@
 import React, { useReducer, createContext } from 'react';
+import userReducer, { initialState } from 'reducers/userReducer';
 
 export const UserContext = createContext();
 
-const initialState = {
-  userInfo: null,
-  loading: false,
-  error: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'SIGN_IN':
-      return {
-        ...state,
-        loading: true,
-      };
-    case 'SIGN_IN_SUCCESS':
-      return {
-        ...state,
-        userInfo: action.payload,
-        loading: false,
-        error: null,
-      };
-    case 'SIGN_IN_FAIL':
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    case 'SIGN_UP':
-      return {
-        ...state,
-        loading: true,
-      };
-    case 'SIGN_UP_SUCCESS':
-      return {
-        ...state,
-        userInfo: action.payload,
-        loading: false,
-        error: null,
-      };
-    case 'SIGN_UP_FAIL':
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    case 'SIGN_OUT':
-      return {
-        ...state,
-        userInfo: null,
-      };
-    case 'CLEAR_ERROR':
-      return {
-        ...state,
-        error: null,
-      };
-    default:
-      throw new Error();
-  }
-};
-
 export const UserContextProvider = props => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(userReducer, initialState);
 
   return (
     <UserContext.Provider value={[state, dispatch]}>
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,58 @@
+export const initialState = {
+  userInfo: null,
+  loading: false,
+  error: null,
+};
+
+export default function userReducer(state, action) {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'SIGN_IN_SUCCESS':
+      return {
+        ...state,
+        userInfo: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'SIGN_IN_FAIL':
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case 'SIGN_UP':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'SIGN_UP_SUCCESS':
+      return {
+        ...state,
+        userInfo: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'SIGN_UP_FAIL':
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case 'SIGN_OUT':
+      return {
+        ...state,
+        userInfo: null,
+      };
+    case 'CLEAR_ERROR':
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      throw new Error();
+  }
+}
