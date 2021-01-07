fd87a1742b7b187dfafdbc650b88e29d0c7e60b6
KostasApi <kapidopo@hotmail.com>
2021-01-07 12:25:11 +0200

    remove log

--- client/src/utils/validateEmail.js
+++ client/src/utils/validateEmail.js
@@ -1,9 +1,5 @@
 const validateEmail = email => {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  console.log(
-    'regex.test(String(email).toLowerCase()); :>> ',
-    regex.test(String(email).toLowerCase())
-  );
   return regex.test(String(email).toLowerCase());
 };
 
